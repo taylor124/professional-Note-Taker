578687372fc9eb6a66f87b829e27dcee206a985e
taylor124 <74930599+taylor124@users.noreply.github.com>
2021-02-19 13:55:41 -0500

    Feat: get post routes

---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static('./Develop/public'));
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
+})
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        const notes = JSON.parse(data)
+        console.log(notes);
+        res.json(notes);
+    })
+
+})
+
+app.post('/api/notes', (req, res) => {
+
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    console.log(newNote);
+
+
+    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        const notes = JSON.parse(data)
+        console.log(notes);
+        notes.push(newNote)
+        fs.writeFile('./Develop/db/db.json', JSON.stringify(notes), (err) => {
+            if (err) {
+                throw err;
+            }
+            res.json(notes);
+        })
+    })
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const deleteNote = req.params.id;
+    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        const notes = JSON.parse(data)
+
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].id == id) {
+                data.splice(i, 1);
+                break;
+            }
+        }
+        console.log(notes);
+        notes.push(newNote)
+        fs.writeFile('./Develop/db/db.json', JSON.stringify(notes), (err) => {
+            if (err) {
+                throw err;
+            }
+            res.json(notes);
+        })
+    })
+
+    console.log(req.params.id);
+
+})
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
