607ca8cb223efe77e1b0743608fea541fa5b0eb1
taylor124 <74930599+taylor124@users.noreply.github.com>
2021-02-21 10:23:30 -0500

    Removed Develop Folder

--- server.js
+++ server.js
@@ -7,22 +7,22 @@ const { v4: uuidv4 } = require('uuid');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.static('./Develop/public'));
+app.use(express.static('./public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             throw err;
         }
@@ -39,14 +39,14 @@ app.post('/api/notes', (req, res) => {
     console.log(newNote);
 
 
-    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             throw err;
         }
         const notes = JSON.parse(data)
         console.log(notes);
         notes.push(newNote)
-        fs.writeFile('./Develop/db/db.json', JSON.stringify(notes), (err) => {
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
             if (err) {
                 throw err;
             }
@@ -58,7 +58,7 @@ app.post('/api/notes', (req, res) => {
 app.delete('/api/notes/:id', (req, res) => {
     const deleteNote = req.params.id;
     console.log(deleteNote);
-    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             throw err;
         }
@@ -71,7 +71,7 @@ app.delete('/api/notes/:id', (req, res) => {
             }
         }
         console.log(notes);
-        fs.writeFile('./Develop/db/db.json', JSON.stringify(notes), (err) => {
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
             if (err) {
                 throw err;
             }
